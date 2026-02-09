7d9e8e148e03f0ac8c22a16179ce1dc3e37319fd
indumakaweeshvara <indumakawee4@gmail.com>
2026-02-09 19:44:37 +0530

    refactor: add components index for centralized UI exports

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Component exports for JobSeeker App
+ * @module components
+ */
+
+export { default as CustomButton } from './CustomButton';
+export { default as InputField } from './InputField';
+export { default as JobCard } from './JobCard';
