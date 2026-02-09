e3a37e822abd309062a25240ae7539d902cf861d
indumakaweeshvara <indumakawee4@gmail.com>
2026-02-09 19:44:01 +0530

    refactor: add services index for centralized Firebase exports

---
+++ src/services/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Services module exports for JobSeeker App
+ * @module services
+ */
+
+export { auth, db, storage } from './firebaseConfig';
+export * from './firestoreActions';
+export { seedJobs } from './seedData';
